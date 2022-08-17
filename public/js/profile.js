d81c66420537d4a3effad2c2e4f732f3479d975b
Manhduy1711 <20010950@st.phenikaa-uni.edu.vn>
2022-08-17 14:42:59 +0700

    delete duy.js

--- public/js/profile.js
+++
@@ -1,9 +0,0 @@
-var arrayNavItem = document.querySelectorAll(".nav-link");
-console.log(arrayNavItem[0]);
-arrayNavItem.forEach(element => {
-    element.addEventListener("click", function() {
-        var current = document.getElementsByClassName("active");
-        current[0].className = current[0].className.replace(" active", "");
-        element.classList.add("active");
-    })
-})
